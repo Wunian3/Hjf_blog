e76e8a1a30dd852aa0fe963514c4ec3f0766cc28
wunian <146049150+Wunian3@users.noreply.github.com>
2025-03-11 20:26:45 +0800

    制表

---
+++ flag/db.go
@@ -0,0 +1,31 @@
+package flag
+
+import (
+	"blog_server/global"
+	"blog_server/models"
+)
+
+func Makemigrations() {
+	var err error
+	global.DB.SetupJoinTable(&models.UserModel{}, "CollectsModels", &models.UserCollectModel{})
+	global.DB.SetupJoinTable(&models.MenuModel{}, "Banners", &models.MenuBannerModel{})
+	err = global.DB.Set("gorm:table_options", "ENGINE=InnoDB").
+		AutoMigrate(
+			&models.BannerModel{},
+			&models.TagModel{},
+			&models.MessModel{},
+			&models.AdtModel{},
+			&models.UserModel{},
+			&models.CommentModel{},
+			&models.ArticleModel{},
+			&models.MenuModel{},
+			&models.MenuBannerModel{},
+			&models.FadeBackModel{},
+			&models.LogDataMd{},
+		)
+	if err != nil {
+		global.Log.Error("[ error ] 生成数据库表结构失败")
+		return
+	}
+	global.Log.Info("[ success ] 生成数据库表结构成功！")
+}
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"blog_server/core"
+	"blog_server/flag"
 	"blog_server/global"
 	"blog_server/router"
+	//"flag"
 	"fmt"
 )
 
@@ -14,8 +16,17 @@ func main() {
 	//connect hblog_db
 	global.DB = core.InitGorm()
 	fmt.Println(global.DB)
+	option := flag.Parse()
+	//fmt.Println(option)
+	if flag.IsWebStop(option) {
+		flag.SwitchOption(option)
+		return
+	}
 	router := router.InitRouter()
 	addr := global.Config.System.Addr()
 	global.Log.Infof("%s ,hjfblog启动！", addr)
-	router.Run(addr)
+	err := router.Run(addr)
+	if err != nil {
+		global.Log.Fatalf(err.Error())
+	}
 }
