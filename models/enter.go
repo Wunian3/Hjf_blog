a9ada4cc95fc2d99e8729338a348e85def9b389c
wunian <146049150+Wunian3@users.noreply.github.com>
2025-03-20 13:12:33 +0800

    debug版本

--- models/enter.go
+++ models/enter.go
@@ -5,7 +5,7 @@ import "time"
 type MODEL struct {
 	ID        uint      `gorm:"primary_key" json:"id"  structs:"-"`
 	UpdatedAt time.Time `json:"-" structs:"-"`
-	CreatedAt time.Time `json:"createdAt" structs:"-"`
+	CreatedAt time.Time `json:"created_at" structs:"-"`
 }
 type PageInf struct {
 	Page  int    `form:"page"`
--- models/user_collect_md.go
+++ models/user_collect_md.go
@@ -4,9 +4,8 @@ import "time"
 
 // 用户收藏表 自己定义用
 type UserCollectModel struct {
-	UserID       uint         `gorm:"primaryKey"`
-	UserModel    UserModel    `gorm:"foreignKey:UserID"`
-	ArticleID    uint         `gorm:"primaryKey"`
-	ArticleModel ArticleModel `gorm:"foreignKey:ArticleID"`
-	CreatedAt    time.Time
+	UserID    uint      `gorm:"primaryKey"`
+	UserModel UserModel `gorm:"foreignKey:UserID"`
+	ArticleID uint      `gorm:"primaryKey"`
+	CreatedAt time.Time
 }
